9296f80d161fb4041f5bc26433c9f01e2b13f9a3
Kavita-Dhamot <kavitadhamot@gmail.com>
2022-12-22 18:40:58 +0530

    created new files

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 $con = mysqli_connect("localhost", "root", "", "social");   //connection to our database(localhost, database username, database password, database name)
 
 if(mysqli_connect_errno()) {
-    echo "Failed to connect: " . mysqli_connect_errno();
+    echo "Failed to connect: " . mysqli_connect_errno(
+        
+    );
 }
 
 $query = mysqli_query($con, "INSERT INTO test VALUES('', 'Kavita')");
