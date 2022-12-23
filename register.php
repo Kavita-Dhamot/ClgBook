a866bffec73dff32108832cdfa15ca5f4d5afba4
Kavita-Dhamot <kavitadhamot@gmail.com>
2022-12-23 20:46:50 +0530

    raw register page

--- register.php
+++ register.php
@@ -89,14 +89,52 @@ if(isset($_POST['register_button'])) {
     if($password != $password2) {
         array_push($error_array, "Your passwords do not match<br>");
     }
+
     else {
         if(preg_match('/[^A-Za-z0-9]/', $password)) {
             array_push($error_array, "Your password can only contain english characters or numbers<br>");
         }
     }
 
-    if(strlen($password > 30 || strlen($password) <5)) {
+    if(strlen($password) > 30 || strlen($password) < 5) {
         array_push($error_array, "Your password must be between 5-30 characters<br>");
+    }
+
+    if(empty($error_array)) {
+        $password = md5($password);   //encrypt the password before sending to database
+
+        //generate username by concatenating first and last name
+        $username = strtolower($fname . "_" . $lname);
+        $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+
+        $i = 0;
+        //if username exists add i to username
+        while(mysqli_num_rows($check_username_query) != 0) {
+            $i++;
+            $username = $username . "_" . $i;
+            $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+        }
+
+        //profile picture assignment
+        $rand = rand(1, 2);  //random number between 1 and 2
+
+        if($rand==1)
+            $profile_pic = "assets/images/profile_pics/defaults/head_deep_blue.png";
+
+        else if($rand==2){
+            $profile_pic = "assets/images/profile_pics/defaults/head_emerald.png";
+        }
+
+        $query = mysqli_query($con, "INSERT INTO users VALUES ('', '$fname', '$lname', '$username', '$em', '$password', '$date', '$profile_pic', '0', '0', 'no', ',')");
+
+        array_push($error_array, "<span style='color: #14C800'> You are all set! Go ahead and login!</span><br>");
+
+        //clear session variables
+        $_SESSION['reg_fname'] = "";
+        $_SESSION['reg_lname'] = "";
+        $_SESSION['reg_email'] = "";
+        $_SESSION['reg_email2'] = "";
+        
     }
 }
 
@@ -141,16 +179,20 @@ if(isset($_POST['register_button'])) {
             <?php if (in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>"; ?>
             <?php if (in_array("Emails don't match<br>", $error_array)) echo "Emails don't match<br>"; ?>
 
-            <input type="text" name="reg_password" placeholder="Password" required>
+            <input type="password" name="reg_password" placeholder="Password" required>
             <br>
-            <input type="text" name="reg_password2" placeholder="Confirm Password" required>
-            <br>
+            <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+            <br> 
             <?php if (in_array("Your passwords do not match<br>", $error_array)) echo "Your passwords do not match<br>"; ?>
             <?php if (in_array("Your password can only contain english characters or numbers<br>", $error_array)) echo "Your password can only contain english characters or numbers<br>"; ?>
             <?php if (in_array("Your password must be between 5-30 characters<br>", $error_array)) echo "Your password must be between 5-30 characters<br>"; ?>
 
 
             <input type="submit" name="register_button" value="Register">
+            <br>
+
+            <?php if (in_array("<span style='color: #14C800'> You are all set! Go ahead and login!</span><br>", $error_array)) echo "<span style='color: #14C800'> You are all set! Go ahead and login!</span><br>"; ?>
+
 
         </form>
     </body>
